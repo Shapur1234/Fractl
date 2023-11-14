a545f239053901b95798d561a15b1b51d469073e
Shapur1234 <48966182+Shapur1234@users.noreply.github.com>
2023-11-14 20:17:05 +0100

    _

--- shell.nix
+++ shell.nix
@@ -1,28 +1,41 @@
-let
-  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
-  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
-  rust_channel = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-in
-with pkgs;
+# let
+#   rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+#   pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+#   rust_channel = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+# in
+# with pkgs;
 
-mkShell rec {
-  nativeBuildInputs = [
-    rust_channel
+# mkShell rec {
+#   nativeBuildInputs = [
+#     rust_channel
 
-    pkg-config
-  ];
-  buildInputs = [
-    alsa-lib
-    libxkbcommon
-    udev
-    vulkan-loader
+#     pkg-config
+#   ];
+#   buildInputs = [
+#     wayland
+
+#     libxkbcommon
+#     xorg.libX11
+#     xorg.libXcursor
+#     xorg.libXi
+#     xorg.libXrandr
+#   ];
+# }
+
+{ pkgs ? import <nixpkgs> { }, lib }:
+pkgs.mkShell {
+  inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
+
+  buildInputs = with pkgs; [
+    clippy
 
     wayland
-
+    libxkbcommon
     xorg.libX11
     xorg.libXcursor
     xorg.libXi
     xorg.libXrandr
   ];
-  LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+
+  # LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
 }
