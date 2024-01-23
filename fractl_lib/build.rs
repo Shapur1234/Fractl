a889c388a75e3ed0c556318a7a6dc9ab889e5846
Shapur1234 <48966182+Shapur1234@users.noreply.github.com>
2024-01-23 21:21:40 +0100

    Improve build script

--- fractl_lib/build.rs
+++ fractl_lib/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if cfg!(feature = "gpu") && cfg!(linux) {
+    if cfg!(feature = "gpu") && std::env::var("CARGO_CFG_TARGET_OS").is_ok_and(|target_os| target_os == "linux") {
         println!("cargo:rustc-link-lib=vulkan");
     }
 }
